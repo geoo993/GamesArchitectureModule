904a39824d7f05f56ab49be07396c954ea93e894
GEORGE QUENTIN <ggeoo93@gmail.com>
2018-02-21 09:59:53 +0000

    initial

---
+++ ChaseCameraGameLab3/ChaseCameraGameLab3/Main.cs
@@ -0,0 +1,28 @@
+﻿#region Using Statements
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using AppKit;
+using Foundation;
+#endregion
+
+namespace ChaseCameraGameLab3.MacOS
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            NSApplication.Init();
+
+            using (ChaseCameraGame game = new ChaseCameraGame())
+            {
+                game.Run();
+            }
+        }
+    }
+    
+}
---
+++ PlatformerLab2/PlatformerLab2/GameInfo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+
+namespace PlatformerLab2.MacOS
+{
+    public class EnemyInfo
+    {
+        public float Speed = 0.0f;
+    }
+    
+    public class GemInfo
+    {
+        public float BounceHeight = 0.0f;
+        public float BounceRate = 0.0f;
+        public Color Color;
+    }
+     
+    public class GameInfo
+    {
+        private static GameInfo mInstance = null;
+        public static GameInfo Instance
+        {
+              get
+              {
+                   if (mInstance == null)
+                   {
+                        mInstance = new GameInfo();
+                   }
+                 return mInstance;
+              }
+             
+              set { mInstance = value; }
+        }
+         
+        public EnemyInfo EnemyInfo;
+        public GemInfo GemInfo;
+    }
+
+
+}
