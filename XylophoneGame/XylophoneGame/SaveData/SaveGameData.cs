71738bb20981e48f758cb5cd7e553f3044a1a914
GEORGE QUENTIN <ggeoo93@gmail.com>
2018-04-12 12:16:06 +0100

    added save data class

---
+++ XylophoneGame/XylophoneGame/SaveData/SaveGameData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using NLua;
+
+    
+namespace XylophoneGame
+{
+    
+    public class SaveGameData
+    {
+        Lua state = new Lua();
+        
+    }
+    
+}
